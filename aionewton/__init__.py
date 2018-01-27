44f1ec3461f424c1e33824baf4a51d0a3b719751
ilevn <nilsntth@gmail.com>
2018-01-27 18:03:29 +0100

    Bump version

--- aionewton/__init__.py
+++ aionewton/__init__.py
@@ -4,7 +4,7 @@ An asnycio-based wrapper for `https://newton.now.sh`
 
 from aionewton.wrapper import AioNewton
 
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 __author__ = "Nils Theres"
 
 __all__ = "AioNewton"
